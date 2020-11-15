21a58af727b552da54d92f93959c7cc04c3c01ed
Rick Silliker <ricksilliker@gmail.com>
2020-11-15 01:43:34 -0600

    first

---
+++ src/components/Card.vue
@@ -0,0 +1,58 @@
+<template>
+<div v-bind:class="cardStyleClass">
+  {{ title }}
+</div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["title", "cardId", "selected", "xPos", "yPos"],
+  data: function() {
+    return {
+      // isSelected: this.selected,
+    }
+  },
+  computed: {
+    cardStyleClass: function() {
+      return {
+        deselectedCard: !this.isSelected,
+        selectedCard: this.isSelected
+      }
+    },
+    isSelected: function() {
+      return this.selected;
+    }
+  },
+  methods: {
+    selectCard: function() {
+      this.isSelected = true;
+    },
+    deselectCard: function() {
+      this.isSelected = false;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.selectedCard {
+  position: absolute;
+  background: mediumseagreen;
+  border: 5px solid goldenrod;
+  color: whitesmoke;
+  border-radius: 5px;
+  width: 200px;
+  height: 150px;
+}
+
+.deselectedCard {
+  position: absolute;
+  background: mediumseagreen;
+  color: whitesmoke;
+  border-radius: 5px;
+  width: 200px;
+  height: 150px;
+}
+</style>
---
+++ src/components/Scene.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="scene" id="scene-graph">
+  <div id="tool-bar">
+    <div id="add-card-btn" v-on:click="addNewCard">Add Card</div>
+  </div>
+  <div id="card-list" v-on:click="deselectCards" v-on:mousemove="onUserMouseMove">
+    <Card v-for="card in cards"
+          :key="card.cardId"
+          v-bind:selected="card.selected"
+          v-bind:title="card.title"
+          v-bind:xPos="card.xPos"
+          v-bind:yPos="card.yPos"
+          v-on:click.native="handleCardSelection(card)"/>
+  </div>
+</div>
+</template>
+
+<script>
+const uuid = require('uuid');
+import Card from "@/components/Card";
+
+export default {
+  name: "Scene",
+  components: {Card},
+  data: function() {
+    return {
+      currentCard: null,
+      cards: [],
+      ctrlModifierPressed: false
+    }
+  },
+  methods: {
+    deselectCards: function(e) {
+      if (e.ctrlKey) {
+        return;
+      }
+      if (e.target.id === "card-list") {
+        this.cards.forEach(c => {
+          c.selected = false;
+        });
+      }
+      this.currentCard = null;
+    },
+    addNewCard: function() {
+      this.cards.push({
+        title: "New Card",
+        cardId: uuid.v4(),
+        selected: false,
+        xPos: 0,
+        yPos: 0,
+      })
+    },
+    handleCardSelection: function(card) {
+      this.cards.forEach(c => {
+        c.selected = false;
+      });
+      card.selected = true;
+      this.currentCard = card;
+    },
+    onUserMouseMove: function(e) {
+      console.log('here');
+      if (e.ctrlKey) {
+        console.log(e.x, e.y);
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.scene {
+  background: #454545;
+  width: 1280px;
+  height: 760px;
+}
+
+#card-list {
+  width: 100%;
+  height: 100%;
+}
+
+#add-card-btn {
+  background: white;
+  border: 1px solid black;
+  color: black;
+  cursor: pointer;
+}
+</style>
